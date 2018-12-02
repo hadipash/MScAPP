7cc3473514131b133a88621555937730d447c2ba
Rustam <dknext99@gmail.com>
2018-12-02 22:17:47 +0800

    modify Main Page

---
+++ app/src/main/java/hk/hku/cs/msccompsc_app/MainPage/SliderAdmission.java
@@ -0,0 +1,24 @@
+package hk.hku.cs.msccompsc_app.MainPage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import hk.hku.cs.msccompsc_app.R;
+
+public class SliderAdmission extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.slider_main, container, false);
+        ImageView img = rootView.findViewById(R.id.slider_img);
+        img.setImageResource(R.drawable.slider2);
+
+        return rootView;
+    }
+}
