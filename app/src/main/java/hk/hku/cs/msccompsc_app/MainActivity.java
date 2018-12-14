5113197cff55d824c768dc0a433d7e6589385ccd
Rustam <dknext99@gmail.com>
2018-12-14 19:17:28 +0800

    small adjustments

--- app/src/main/java/hk/hku/cs/msccompsc_app/MainActivity.java
+++ app/src/main/java/hk/hku/cs/msccompsc_app/MainActivity.java
@@ -86,28 +86,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -130,7 +108,8 @@ public class MainActivity extends AppCompatActivity
             Log.d(TAG, "GA clicked.");
             Intent intent_ga = new Intent(this, GAActivity.class);
             startActivity(intent_ga);
-
+        } else if (id == R.id.nav_contact_us){
+            startActivity(new Intent(this, ContactUs.class));
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
